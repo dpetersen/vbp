0eb6de00b03ed462af7aab66f5c6af904181ba36
Don Petersen <don@donpetersen.net>
2015-03-22 12:09:07 -0700

    WIP totally jacked collision.
    Welllllllll that doesn't work for shit. It (seemingly) randomly lets the
    ball bounce when the paddle is on the other side of the screen. I got
    nothin'. Here's the original commit message I wrote when I thought this
    worked, in a simpler time like 10 minutes ago.
    
    -----------
    
    Partially working smarter collision detection.
    
    It turns out that this is the hardest problem in computer science. Who
    knew. This is busted, as you can see in the TODO. It randomly (but I'm
    sure not actually random) said there was a y-axis collision. Gameplay
    dictates that you can play while ignoring all collisions on that axis
    (except for the walls, which can be detected simply and separately).
    You'll only notice this is jacked if the ball passes through the
    top/bottom of a paddle. It'll look dumb, but won't affect the score
    since that is a point lost or scored regardless.

--- src/game_controller.rs
+++ src/game_controller.rs
@@ -1,4 +1,5 @@
 use std::num::Float;
+use std::f64::{INFINITY, NEG_INFINITY};
 use std::f64::consts::PI;
 
 use sdl2::event::Event;
@@ -102,39 +103,58 @@ impl GameController {
     // Thanks StackOverflow!
     // http://stackoverflow.com/questions/5830791/find-radians-reflection-angle
     fn move_ball(&mut self) {
-        let x_change = (self.ball_angle.cos() * BALL_VELOCITY) as i32;
-        let y_change = (self.ball_angle.sin() * BALL_VELOCITY) as i32;
+        let player_paddle_x = PADDLE_WALL_PADDING;
+        let opponent_paddle_x = self.window_width - PADDLE_WIDTH - PADDLE_WALL_PADDING;
+        let vx = (self.ball_angle.cos() * BALL_VELOCITY) as i32;
+        let vy = (self.ball_angle.sin() * BALL_VELOCITY) as i32;
 
         let (mut x, mut y) = self.ball_position;
-        x += x_change;
-        y += y_change;
 
         // Wall collision
         let upper_y_limit = self.window_height - BALL_BREADTH;
-        if y > upper_y_limit || y < 0 {
+        if y + vy > upper_y_limit || y + vy < 0 {
+            x += vx;
+            y += vy;
+
+            if y > upper_y_limit { y = upper_y_limit; }
+            else if y < 0 { y = 0; }
+
             self.play_sound(&self.boop);
-            self.ball_angle *= -1.0
+            self.ball_angle *= -1.0;
+
+            self.ball_position = (x, y);
+            return;
         }
-        if y > upper_y_limit { y = upper_y_limit; }
-        else if y < 0 { y = 0; }
 
-        // Paddle collision
-        // TODO ball dimensions aren't respected. Left side of ball hits on the left, right side of
-        // ball hits on the right. Also need to reset ball x on impact.
-        let player_paddle_x = PADDLE_WALL_PADDING + PADDLE_WIDTH;
-        let player_paddle_impacted = x <= player_paddle_x && y + BALL_BREADTH >= self.player_paddle_y && y <= self.player_paddle_y + PADDLE_HEIGHT;
-        let opponent_paddle_x = self.window_width - PADDLE_WALL_PADDING - PADDLE_WIDTH - BALL_BREADTH;
-        let opponent_paddle_impacted = x >= opponent_paddle_x && y >= self.opponent_paddle_y && y <= self.opponent_paddle_y + PADDLE_HEIGHT;
+        let (p_normal_x, p_normal_y, p_collision_time) = self.swept_aabb(vx, vy, player_paddle_x, self.player_paddle_y);
+        let (o_normal_x, o_normal_y, o_collision_time) = self.swept_aabb(vx, vy, opponent_paddle_x, self.opponent_paddle_y);
 
-        // Reflect and tweak ball position.
-        if player_paddle_impacted || opponent_paddle_impacted {
-            self.play_sound(&self.boop);
-            self.ball_angle = 0.0 - PI - self.ball_angle; 
+        // TODO You haven't accounted for remainingtime, like they do in the example. That would be
+        // more accurate since there wouldn't be unaccounted-for velocity loss on an impact frame.
+        if p_collision_time < 1.0 || o_collision_time < 1.0 {
+            if p_normal_x != 0.0 || o_normal_x != 0.0 {
+                self.play_sound(&self.boop);
+                self.ball_angle = 0.0 - PI - self.ball_angle; 
+
+                let mut time: f64;
+                if p_collision_time < 1.0  { time = p_collision_time; }
+                else { time = o_collision_time; }
+
+                x += (vx as f64 * time) as i32;
+                y += (vy as f64 * time) as i32;
+            } else {
+                // TODO there's a bug here where your y collision is firing when the ball is far
+                // far away from any paddles. Seemingly always the opponent paddle, but the x
+                // position can be on any part of the screen when it bounces back.
+                x += (vx as f64 * 1.0) as i32;
+                y += (vy as f64 * 1.0) as i32;
+            }
+        } else {
+            x += vx;
+            y += vy;
         }
-        if player_paddle_impacted { x = player_paddle_x; }
-        else if opponent_paddle_impacted { x = opponent_paddle_x; }
 
-        if x < player_paddle_x || x > opponent_paddle_x {
+        if x < 0 || x > self.window_width {
             if x < player_paddle_x { self.opponent_score += 1; }
             else if x > opponent_paddle_x { self.player_score += 1; }
 
@@ -142,6 +162,80 @@ impl GameController {
             self.restart_game();
         } else {
             self.ball_position = (x, y);
+        }
+    }
+
+    // Stolen and partially understood from:
+    // http://www.gamedev.net/page/resources/_/technical/game-programming/swept-aabb-collision-detection-and-response-r3084
+    fn swept_aabb(&self, vx: i32, vy: i32, paddle_x: i32, paddle_y: i32) -> (f64, f64, f64) {
+        let (ball_x, ball_y) = self.ball_position;
+
+        let mut x_inv_entry: i32;
+        let mut y_inv_entry: i32;
+        let mut x_inv_exit: i32;
+        let mut y_inv_exit: i32;
+
+        // find the distance between the objects on the near and far sides for both x and y
+        if vx > 0 {
+            x_inv_entry = paddle_x - (ball_x + BALL_BREADTH);
+            x_inv_exit = (paddle_x + PADDLE_WIDTH) - ball_x;
+        } else {
+            x_inv_entry = (paddle_x + PADDLE_WIDTH) - ball_x;
+            x_inv_exit = paddle_x - (ball_x + BALL_BREADTH);
+        }
+
+        if vy > 0 {
+            y_inv_entry = paddle_y - (ball_y + BALL_BREADTH);
+            y_inv_exit = (paddle_y + PADDLE_HEIGHT) - ball_y;
+        } else {
+            y_inv_entry = (paddle_y + PADDLE_HEIGHT) - ball_y;
+            y_inv_exit = paddle_y - (ball_y + BALL_BREADTH);
+        }
+
+
+        let mut x_entry: f64;
+        let mut y_entry: f64;
+        let mut x_exit: f64;
+        let mut y_exit: f64;
+
+        // find time of collision and time of leaving for each axis
+        if vx == 0 {
+            x_entry = NEG_INFINITY;
+            x_exit = INFINITY;
+        } else {
+            x_entry = x_inv_entry as f64 / vx as f64;
+            x_exit = x_inv_exit as f64 / vx as f64;
+        }
+
+        if vy == 0 {
+            y_entry = NEG_INFINITY;
+            y_exit = INFINITY;
+        } else {
+            y_entry = y_inv_entry as f64 / vy as f64;
+            y_exit = y_inv_exit as f64 / vy as f64;
+        }
+
+        // find the earliest/latest times of collision
+        let entry_time = x_entry.max(y_entry);
+        let exit_time = x_exit.max(y_exit);
+
+        if entry_time > exit_time || (x_entry < 0.0 && y_entry < 0.0) || x_entry > 1.0 || y_entry > 1.0 {
+            // there was no collision
+            return (0.0, 0.0, 1.0);
+        } else {
+            if x_entry > y_entry {
+                if x_inv_entry < 0 {
+                    return (1.0, 0.0, entry_time);
+                } else {
+                    return (-1.0, 0.0, entry_time);
+                }
+            } else {
+                if y_inv_entry < 0 {
+                    return (0.0, 1.0, entry_time);
+                } else {
+                    return (0.0, -1.0, entry_time);
+                }
+            }
         }
     }
 
